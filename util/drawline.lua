2641f6bb9dc5e3029d466ad6e295a4d9a411f7ad
rech <rech@disroot.org>
2023-05-21 23:09:12 +0800

    DrawLine: set timing to timing-1 for viewing arcs

--- util/drawline.lua
+++ util/drawline.lua
@@ -1,10 +1,15 @@
 do
+    ---@type rech.dialogs.Dialog
+    local Dialog = require("rech.dialogs.Dialog")
+    ---@type rech.dialogs.Dropdown
+    local Dropdown = require("rech.dialogs.fields.Dropdown")
     ---@module rech.util.DrawLine
     local this = {}
     this.__index = this
 
     local __MACRO_ID = "rech.util.drawline"
     local __MACRO_DISPLAY_NAME = "Draw arc by clicking"
+    local __MACRO_DIALOG_TITLE = "Draw arc configuration"
 
     ---@type rech.lib.Request
     local request = require("rech.lib.request")
@@ -20,6 +25,9 @@ do
     function this.activate()
         local timing = request.Timing(true, "Select timing")
         local pos1 = request.VerticalPosition(timing, "Select starting position")
+        if Context.currentTiming == timing then
+            Context.currentTiming = timing-1
+        end
         while true do
             local pos2 = request.VerticalPosition(timing, "Select next position or cancel")
             local event = Event.arc(timing, pos1, timing, pos2, Context.currentIsTraceMode, Context.currentArcColor,  "s", Context.currentTimingGroup, "none")
