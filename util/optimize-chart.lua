724c4cef9a243ace0fd326069f95d85da04a7468
rech <rech@disroot.org>
2023-06-09 00:33:32 +0800

    Improve OptimizeChart macro

--- util/optimize-chart.lua
+++ util/optimize-chart.lua
@@ -20,9 +20,15 @@ do
 
     ---@param batch Command
     local function optimizeStraightArc(batch)
-        local q = jq.query("arc[x1==x2,y1==y2] arc[t1==t2]")
+        local q = nil
+        q = jq.query("arc[x1==x2,y1==y2] arc[t1==t2] arc[x1==x2,y1~=y2,easing=si] arc[x1==x2,y1~=y2,easing=so]")
         for _,v in ipairs(q.events.arc) do
             v.type = "s"
+            batch.add(v.save())
+        end
+        q = jq.query("arc[y1==y2]")
+        for _,v in ipairs(q.events.arc) do
+            v.type = v.type:sub(0, 2)
             batch.add(v.save())
         end
     end
